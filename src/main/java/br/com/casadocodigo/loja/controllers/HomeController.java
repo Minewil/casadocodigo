7ffe784ce0afce7398d588423562a8e57341ffed
Wiliam Hatimine <wiliamhatimine@gmail.com>
2017-02-13 21:26:21 -0200

    commitando localmente

--- src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
@@ -1,9 +1,12 @@
 package br.com.casadocodigo.loja.controllers;
 
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Transactional
 public class HomeController {
 
 	@RequestMapping("/")
--- src/main/java/br/com/casadocodigo/loja/controllers/ProductsController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProductsController.java
@@ -1,7 +1,16 @@
 package br.com.casadocodigo.loja.controllers;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.casadocodigo.loja.daos.ProductDAO;
+import br.com.casadocodigo.loja.models.Product;
+
+@Controller
+@Transactional
 public class ProductsController {
 	
 	private ProductDAO productDAO;
